90b255301abc468a6d1e59083fb082178ab0688c
hatcyl <hatcyl@gmail.com>
2023-02-28 21:45:27 -0800

    Start implementation for FRP JobQueue data structure.

---
+++ hatcyl.DataStructures.Frp.Tests/JobQueueTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hatcyl.DataStructures.Frp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sodium.Frp;
+using Sodium.Functional;
+using System.Collections.Immutable;
+using Stream = Sodium.Frp.Stream;
+
+namespace hatcyl.DataStructures.Frp.Tests
+{
+    [TestClass()]
+    public class JobQueueTests
+    {
+        [TestMethod()]
+        public void JobQueueTest()
+        {
+            IImmutableQueue<string> initalState = ImmutableQueue.Create<string>().Enqueue("Initial");
+            StreamSink<Unit> clearStream = Stream.CreateSink<Unit>();
+            StreamSink<string> completeStream = Stream.CreateSink<string>();
+            StreamSink<Unit> dequeStream = Stream.CreateSink<Unit>();
+            StreamSink<string> enqueueStream = Stream.CreateSink<string>();
+
+            JobQueue<string> stringQueue = new JobQueue<string>
+            (
+                new QueueBuilder<string>(),
+                new ListBuilder<string>(),
+                clearStream,
+                completeStream,
+                dequeStream,
+                enqueueStream
+            );
+
+            stringQueue.List.Enumerable.Listen(x => { foreach (var value in x) Console.WriteLine(value); });
+
+            enqueueStream.Send("One");
+            enqueueStream.Send("Two");
+            dequeStream.Send(Unit.Value);
+            dequeStream.Send(Unit.Value);
+            enqueueStream.Send("Three");
+            dequeStream.Send(Unit.Value);
+        }
+    }
+}
---
+++ hatcyl.DataStructures.Frp/JobQueue.cs
@@ -0,0 +1,49 @@
+﻿using hatcyl.DataStructures.Frp.Utilities;
+using Sodium.Frp;
+using Sodium.Functional;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hatcyl.DataStructures.Frp;
+public class JobQueue<T>
+{
+    public Queue<T> Queue { get; }
+    public List<T> List { get; }
+    public Stream<Unit> ClearStream { get; }
+    public Stream<T> CompleteStream { get; }
+    public Stream<Unit> DequeStream { get; }
+    public Stream<T> EnqueueStream { get; }
+
+    public JobQueue
+    (
+        QueueBuilder<T> queueBuilder,
+        ListBuilder<T> listBuilder,
+        Stream<Unit> clearStream,
+        Stream<T> completeStream,
+        Stream<Unit> dequeStream,
+        Stream<T> enqueueStream
+    )
+    {
+        Queue = (queueBuilder with
+        {
+            EnqueueStream = enqueueStream,
+            DequeStream = dequeStream,
+            ClearStream = clearStream,
+        }).Build();
+
+        List = (listBuilder with
+        {
+            AddStream = dequeStream.Snapshot(Queue.Peek).FilterMaybe()
+        }).Build();
+        
+        ClearStream = clearStream;
+        CompleteStream = completeStream;
+        DequeStream = dequeStream;
+        EnqueueStream = enqueueStream;
+    }
+}
+
